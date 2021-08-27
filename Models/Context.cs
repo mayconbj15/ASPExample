a48c37a10894de973d7a3d7b16bd20e50367cbde
mayconbj15 <mayconbj15@gmail.com>
2021-08-27 00:45:11 -0300

    add categories crud

---
+++ Models/Context.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPExample.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Category> Categories {get;set;}
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseSqlServer("Server=NOOBMANBR-DESKT\\SQLEXPRESS;Database=CursoMVC;Integrated Security=True;");
+        }
+    }
+}
